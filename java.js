e1af1de2a7eb80d24e3ebcc8dd5fe1235a5df335
Coven15 <133569737+Cc0v3n@users.noreply.github.com>
2025-06-01 09:37:08 +0300

    Update java.js

--- java.js
+++ java.js
@@ -1,6 +1,6 @@
 startbutton = document.querySelector('.button1')
 
 startbutton.addEventListener('click', function() {
-    window.location.href = 'https://av1nate.github.io/Pixel/';
+    window.location.href = 'https://cc0v3n.github.io/Pixel/';
 
 })
